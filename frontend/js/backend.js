1198ad5e20ad8b49a9edf6f08ceec8cfd0b567e3
fdsdev90@gmail.com <fdsdev90@gmail.com>
2020-04-26 22:26:52 +0200

    fix backend.js

--- frontend/js/backend.js
+++ frontend/js/backend.js
@@ -4,8 +4,15 @@ function checkJoke()
 {
 
   let jokeType = $("#jokeType").attr("value");
-  axios.get('http://localhost:8080/joke/'+jokeType).then((response)=>{console.log(response);})
-  //axios.get('').then();
+  axios.get('http://localhost:8080/joke/'+jokeType).then
+  (
+    (response)=>
+    {
+      console.log(response);
+    }
+
+  );
+
 
 
 }
